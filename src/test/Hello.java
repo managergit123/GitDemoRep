acc599ca02614bcf0052380b62f7b3b42aa0a4df
mbashir1 <mbashir1@N5107S41-INST.ad.douglascollege.ca>
2025-04-14 12:15:56 -0700

    First commit

---
+++ src/test/Hello.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This code is by Manager");
+		System.out.println("This project is created by manager");
+		System.out.println("Add more code");
+	}
+
+}
