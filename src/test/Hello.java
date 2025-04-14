dec9d29388a6294433cf44b11b228dbd55b0f533
mbashir1 <mbashir1@N5107S41-INST.ad.douglascollege.ca>
2025-04-14 12:51:05 -0700

    Commit by team member

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -7,6 +7,8 @@ public class Hello {
 		System.out.println("This code is by Manager");
 		System.out.println("This project is created by manager");
 		System.out.println("Add more code");
+		System.out.println("This code is by team member");
+		System.out.println("Hello from Team member");
 	}
 
 }
